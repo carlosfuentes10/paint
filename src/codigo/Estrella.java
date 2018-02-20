5d0fd2b1c483d494cf97f427658ae5fd588cfa44
Jorge Cisneros <Jorge@Cetys>
2018-02-20 07:30:14 +0100

    Con una estrella de la Serendipia hipervitaminada

--- src/codigo/Estrella.java
+++ src/codigo/Estrella.java
@@ -14,19 +14,22 @@ public class Estrella extends Forma {
 
     public Estrella(int _posX, int _posY, Color _color, boolean _relleno) {
         //inicializa el constructor del triángulo correctamente para que guarde 3 lados
-        super(_posX, _posY, 32, _color, _relleno);
+        super(_posX, _posY, 1024, _color, _relleno);
     }
 
     
     @Override 
     public void calculaVertices(int _radio, double _giro){
+        int n = 20; 
         for(int i=0; i<npoints; i++){
-            this.xpoints[i] = (int) (x + _radio*Math.cos((2*Math.PI * i + _giro)/npoints));
-            this.xpoints[i+1] = (int) (x + (_radio/2)*Math.cos((2*Math.PI * i + _giro/2)/npoints));
+            //this.xpoints[i] = (int) (x + _radio*Math.cos((2*Math.PI * i + _giro)/npoints));
+            this.xpoints[i] = (int) (x + (_radio/(i%n +1))*Math.cos((2*Math.PI * i + _giro/(i%n +1))/npoints));
+            //this.xpoints[i+2] = (int) (x + (_radio/3)*Math.cos((2*Math.PI * i + _giro/3)/npoints));
             
-            this.ypoints[i] = (int) (y + _radio*Math.sin((2*Math.PI * i + _giro)/npoints));
-            this.ypoints[i+1] = (int) (y + (_radio/2)*Math.sin((2*Math.PI * i + _giro/2)/npoints));
-            i++;
+            //this.ypoints[i] = (int) (y + _radio*Math.sin((2*Math.PI * i + _giro)/npoints));
+            this.ypoints[i] = (int) (y + (_radio/(i%n +1))*Math.sin((2*Math.PI * i + _giro/(i%n +1))/npoints));
+            //this.ypoints[i+2] = (int) (y + (_radio/3)*Math.sin((2*Math.PI * i + _giro/3)/npoints));
+            //i++;i++;
         }
     }
     
